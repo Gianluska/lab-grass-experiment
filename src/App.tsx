243d5bec219910fa81bf131a6bd7511a6697b6bc
Gianlucca Claudino <gianluccaclaudino@gmail.com>
2024-10-02 22:25:41 -0300

    refactor: Update Home component with camera position and field of view

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,12 @@ import "./index.css";
 function App() {
   return (
     <div className="w-full h-screen">
-      <Canvas className="bg-slate-950" dpr={[1, 2]} shadows>
+      <Canvas
+        className="bg-slate-950"
+        dpr={[1, 2]}
+        shadows
+        camera={{ position: [0, 5, 15], fov: 60 }}
+      >
         <Home />
       </Canvas>
     </div>

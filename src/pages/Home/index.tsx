eb52c7e26fd9e7d218bc62741eeab7ab01ce0925
Gianlucca Claudino <gianluccaclaudino@gmail.com>
2024-10-11 00:07:17 -0300

    refactor: Update Home component to include SSAO pass and adjust sky rendering

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,14 @@
 import { Grass } from "@components/Grass";
-import { OrbitControls, Sky, Stars, Stats } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
+import { OrbitControls, Sky, Stats } from "@react-three/drei";
+import { extend, useFrame } from "@react-three/fiber";
 import { useControls } from "leva";
 import { Suspense } from "react";
+import { SSAOPass } from "three-stdlib"
+
+extend({ SSAOPass });
 
 export function Home() {
-  const { cameraPosition } = useControls('Camera', {
+  const { cameraPosition } = useControls("Camera", {
     cameraPosition: {
       value: { x: 8.6, y: -0.8, z: -7.8 },
       step: 0.1,
@@ -15,15 +18,10 @@ export function Home() {
   useFrame(({ camera }) => {
     camera.position.set(cameraPosition.x, cameraPosition.y, cameraPosition.z);
   });
-  
+
   return (
     <>
-      <Sky
-        distance={50}
-        sunPosition={[1, 1, 0]}
-        inclination={0}
-      />
-      <Stars fade depth={200} />
+      <Sky distance={50} sunPosition={[1, 1, 0]} azimuth={0.25} />
 
       <ambientLight intensity={0.1} color="#ffffff" />
 
